065e515cab915fd16781181079ef727e789d7c92
atomic <phutthawath@gmail.com>
2026-05-03 10:54:31 +0700

    fix capture

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ if not os.path.exists("Known_faces"):
 name = input("Enter your name and surname: ")
 student_ID = input("Enter your student ID: ")
 video_capture = cv2.VideoCapture(0)
-print("look at the camera and press q for capture or press q for exit")
+print("look at the camera and press c for capture or press q for exit")
 
 while True:
     ret, frame = video_capture.read()
     cv2.imshow("Registering", frame)
     key = cv2.waitKey(1)
-    if key & 0xFF == ord("s"):
+    if key & 0xFF == ord("c"):
         filename = f"Known_faces/{name}_{student_ID}.jpg"
         cv2.imwrite(filename, frame)
         print(f"Image saved as {filename} finish!")
